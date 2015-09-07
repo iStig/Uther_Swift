27d4369c879ccb96d980ac024bb0331ca095cb8c
Jian Ming Zhang <smartzjm@gmail.com>
2015-09-07 16:02:51 +0800

    pod install

---
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -0,0 +1,113 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// Alamofire
+#define COCOAPODS_POD_AVAILABLE_Alamofire
+#define COCOAPODS_VERSION_MAJOR_Alamofire 1
+#define COCOAPODS_VERSION_MINOR_Alamofire 3
+#define COCOAPODS_VERSION_PATCH_Alamofire 1
+
+// CryptoSwift
+#define COCOAPODS_POD_AVAILABLE_CryptoSwift
+#define COCOAPODS_VERSION_MAJOR_CryptoSwift 0
+#define COCOAPODS_VERSION_MINOR_CryptoSwift 0
+#define COCOAPODS_VERSION_PATCH_CryptoSwift 13
+
+// GCD
+#define COCOAPODS_POD_AVAILABLE_GCD
+#define COCOAPODS_VERSION_MAJOR_GCD 0
+#define COCOAPODS_VERSION_MINOR_GCD 2
+#define COCOAPODS_VERSION_PATCH_GCD 1
+
+// JSQMessagesViewController
+#define COCOAPODS_POD_AVAILABLE_JSQMessagesViewController
+#define COCOAPODS_VERSION_MAJOR_JSQMessagesViewController 7
+#define COCOAPODS_VERSION_MINOR_JSQMessagesViewController 1
+#define COCOAPODS_VERSION_PATCH_JSQMessagesViewController 0
+
+// JSQSystemSoundPlayer
+#define COCOAPODS_POD_AVAILABLE_JSQSystemSoundPlayer
+#define COCOAPODS_VERSION_MAJOR_JSQSystemSoundPlayer 2
+#define COCOAPODS_VERSION_MINOR_JSQSystemSoundPlayer 0
+#define COCOAPODS_VERSION_PATCH_JSQSystemSoundPlayer 1
+
+// KeyboardMan
+#define COCOAPODS_POD_AVAILABLE_KeyboardMan
+#define COCOAPODS_VERSION_MAJOR_KeyboardMan 0
+#define COCOAPODS_VERSION_MINOR_KeyboardMan 4
+#define COCOAPODS_VERSION_PATCH_KeyboardMan 0
+
+// KeychainAccess
+#define COCOAPODS_POD_AVAILABLE_KeychainAccess
+#define COCOAPODS_VERSION_MAJOR_KeychainAccess 1
+#define COCOAPODS_VERSION_MINOR_KeychainAccess 2
+#define COCOAPODS_VERSION_PATCH_KeychainAccess 1
+
+// LTMorphingLabel
+#define COCOAPODS_POD_AVAILABLE_LTMorphingLabel
+#define COCOAPODS_VERSION_MAJOR_LTMorphingLabel 0
+#define COCOAPODS_VERSION_MINOR_LTMorphingLabel 0
+#define COCOAPODS_VERSION_PATCH_LTMorphingLabel 7
+
+// Moya
+#define COCOAPODS_POD_AVAILABLE_Moya
+#define COCOAPODS_VERSION_MAJOR_Moya 2
+#define COCOAPODS_VERSION_MINOR_Moya 1
+#define COCOAPODS_VERSION_PATCH_Moya 0
+
+// Moya/Core
+#define COCOAPODS_POD_AVAILABLE_Moya_Core
+#define COCOAPODS_VERSION_MAJOR_Moya_Core 2
+#define COCOAPODS_VERSION_MINOR_Moya_Core 1
+#define COCOAPODS_VERSION_PATCH_Moya_Core 0
+
+// SQLite.swift
+#define COCOAPODS_POD_AVAILABLE_SQLite_swift
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.0.pre.
+
+// SQLite.swift/Core
+#define COCOAPODS_POD_AVAILABLE_SQLite_swift_Core
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.0.pre.
+
+// SQLite.swift/Library
+#define COCOAPODS_POD_AVAILABLE_SQLite_swift_Library
+// This library does not follow semantic-versioning,
+// so we were not able to define version macros.
+// Please contact the author.
+// Version: 0.1.0.pre.
+
+// SwiftColors
+#define COCOAPODS_POD_AVAILABLE_SwiftColors
+#define COCOAPODS_VERSION_MAJOR_SwiftColors 0
+#define COCOAPODS_VERSION_MINOR_SwiftColors 2
+#define COCOAPODS_VERSION_PATCH_SwiftColors 1
+
+// SwiftDate
+#define COCOAPODS_POD_AVAILABLE_SwiftDate
+#define COCOAPODS_VERSION_MAJOR_SwiftDate 1
+#define COCOAPODS_VERSION_MINOR_SwiftDate 0
+#define COCOAPODS_VERSION_PATCH_SwiftDate 11
+
+// SwiftyJSON
+#define COCOAPODS_POD_AVAILABLE_SwiftyJSON
+#define COCOAPODS_VERSION_MAJOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_MINOR_SwiftyJSON 2
+#define COCOAPODS_VERSION_PATCH_SwiftyJSON 1
+
+// XCGLogger
+#define COCOAPODS_POD_AVAILABLE_XCGLogger
+#define COCOAPODS_VERSION_MAJOR_XCGLogger 2
+#define COCOAPODS_VERSION_MINOR_XCGLogger 3
+#define COCOAPODS_VERSION_PATCH_XCGLogger 0
+
